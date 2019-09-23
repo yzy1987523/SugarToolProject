9aa0c819300a587b03561556c1b3672f2c429d5e
迟谋 <705359976@qq.com>
2019-09-23 17:01:46 +0800

    测试dll

--- Assets/_Use/DLL/DllTest.cs
+++ Assets/_Use/DLL/DllTest.cs
@@ -8,31 +8,31 @@ using System.Reflection;
 
 public class DllTest : MonoBehaviour
 {
-    [DllImport("Dll2", EntryPoint = "Add")]
-    extern static int Add(int a, int b);
+    [DllImport("Dll1", EntryPoint = "yzy")]
+    extern static int yzy(char a);
 
     // Start is called before the first frame update
     void Start()
     {
-        int num = Add(1, 1);
-        Debug.Log(num);
+        //int num = Add(1, 1);
+        Debug.Log(yzy('1'));
     }
-    void T1()
-    {
-        var p = @"D:\Document\Unity\TestDll\bin\Release\TestDll.dll";
-        byte[] bin;
-        using (var fs = new FileStream(p, FileMode.Open))
-        {
-            using (var br = new BinaryReader(fs))
-            {
-                bin = br.ReadBytes(Convert.ToInt32(fs.Length));
-            }
-        }
-        var assembly = Assembly.Load(bin);
+    //void T1()
+    //{
+    //    var p = @"D:\Document\Unity\TestDll\bin\Release\TestDll.dll";
+    //    byte[] bin;
+    //    using (var fs = new FileStream(p, FileMode.Open))
+    //    {
+    //        using (var br = new BinaryReader(fs))
+    //        {
+    //            bin = br.ReadBytes(Convert.ToInt32(fs.Length));
+    //        }
+    //    }
+    //    var assembly = Assembly.Load(bin);
 
-        var t = assembly.GetType("TestDll.test");
-        var instance = Activator.CreateInstance(t);
-        var m = t.GetMethod("add");
-        Debug.Log(m.Invoke(instance, new object[] { 1, 2 }));
-    }
+    //    var t = assembly.GetType("TestDll.test");
+    //    var instance = Activator.CreateInstance(t);
+    //    var m = t.GetMethod("add");
+    //    Debug.Log(m.Invoke(instance, new object[] { 1, 2 }));
+    //}
 }
